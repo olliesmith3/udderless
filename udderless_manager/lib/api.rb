73645a02b1d24fd6e0cd1ac87afb9a5fe9935851
Ollie <oliverdanielsmith@hotmail.com>
2021-01-08 15:44:04 +0000

    Playing with Mapbox api

--- udderless_manager/lib/api.rb
+++ udderless_manager/lib/api.rb
@@ -1,7 +1,11 @@
 require 'net/http'
 require 'json'
 
-uri = URI('https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=ENV['MAPBOX_API_KEY']')
+search = "28 foulser road"
+formatted_search = search.gsub(/" "/, "%20")
+
+url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + formatted_search + ".json?types=address&bbox=-0.599428,51.306690,-0.002237,51.579567&access_token=" + ENV["MAPBOX_API_TOKEN"]
+uri = URI(url)
 res = Net::HTTP.get(uri) 
 res_json = JSON.parse(res)
 
